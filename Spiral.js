9c01fcad67ce978f6253cfa3efbe0d1b83baa708
Michael Mills <michaelmillstech@gmail.com>
2018-05-11 11:28:35 -0400

    Spiral function generates container for Spiral

--- Spiral.js
+++ Spiral.js
@@ -1,12 +1,12 @@
 /*#1: Write a program that accepts a number and produces a spiral of numbers starting at its center, going outward that is equal in length to 2^n, where n is the input number.
 
 Pseudo-code:
-If input is not a whole number, return "please enter a whole number higher than 1 to draw a spiral."
-If input is negative, return "please enter a positive whole number higher than 1 to draw a spiral."
-If input is 0, return "No spiral to draw. Enter a whole number higher than 1 to draw a spiral.""
-If input is 1, return "1 \n Enter a whole number higher than 1 to draw a spiral."
+If input is not a whole number, or less than 2, return "Not a valid number." and ask for user input again.
 
-Create object that will contain spiral.
+If input is a valid number, execute the function that draws the spiral. Once spiral drawing is complete, ask user for another valid number.
+
+If user types 'exit' or 'close', exit the program.
+
 Create variable containing input as 2^n where n === arg.
 
 */
@@ -37,8 +37,12 @@ var askq = function(){
     });
 };
 
-var spiral = function() {
-
+var spiral = function(n) {
+    var container = {}; //Object used to store the spiral
+    for(var i = 0; i < n; i++){ //Loop that generates arrays holding the spiral. # of arrays needed === n
+        container[i] = [];
+    }
+    console.log(container);
 }
 
 askq();
